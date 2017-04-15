31db140068f61149632d3b653f4f8a1a57f5ef05
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-04-15 18:15:22 +0200

    feat: cice counter

--- src/ConverterHeader.vue
+++ src/ConverterHeader.vue
@@ -4,6 +4,9 @@
       Le convertisseur
       <span class="c--red">CICE</span>
     </h1>
+    <p class="c-converter-header__intro">
+      Le CICE coûte cher (<span class="c--red" v-html="ciceCostHtml"></span> depuis que vous avez ouvert ce site). Voici un convertisseur qui vous permet de réinvestir les fonds du CICE vers des recrutements dans les secteurs publics.
+    </p>
   </div>
 </template>
 
@@ -18,8 +21,15 @@
   color: $flashyBlue;
   font-size: 42px;
   line-height: 54px;
+  margin-bottom: 0;
   padding: 0 10px;
   text-align: center;
+}
+
+.c-converter-header__intro {
+  font-family: 'Montserrat', sans-serif;
+  margin: 0 0 40px 0;
+  padding: 0 30px;
 }
 
 @media (max-width: 500px) {
@@ -28,3 +38,34 @@
   }
 }
 </style>
+
+<script>
+import { totalCICE } from './joblist'
+
+export default {
+  data() {
+    return {
+      ciceCost: 0,
+      cicePerSec: totalCICE / (365 * 24 * 60 * 60)
+    }
+  },
+
+  computed: {
+    ciceCostHtml() {
+      return `${this.$options.filters.bigNumber(Math.round(this.ciceCost))}€`
+    }
+  },
+
+  mounted() {
+    setTimeout(() => this.updateCiceCost(), 1000)
+  },
+
+  methods: {
+    updateCiceCost() {
+      this.ciceCost += this.cicePerSec
+
+      setTimeout(() => this.updateCiceCost(), 1000)
+    }
+  }
+}
+</script>
