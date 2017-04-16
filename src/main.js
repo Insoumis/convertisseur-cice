32efe609e55c9368a1f68d3a07a2a285e0b86766
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-04-16 11:10:42 +0200

    fix: number commas and spaces

--- src/main.js
+++ src/main.js
@@ -6,23 +6,14 @@ import 'typeface-roboto-slab'
 import 'normalize.css'
 
 Vue.filter('bigNumber', (jobs) => {
-  jobs = jobs.toString()
-
-  const chunks = []
-  let i = jobs.length - 1
-  while (chunks.length * 3 < jobs.length) {
-    chunks.push((jobs[i - 2] || '') + (jobs[i - 1] || '') + jobs[i])
-    i = i - 3
-  }
-
-  return chunks.reverse().join('&nbsp;')
+  return parseFloat(jobs.toFixed(2)).toLocaleString()
 })
 
 Vue.filter('billions', (number) => {
   const n = (number < 10e8) ? number / 10e5 : number / 10e8
   const w = (number < 10e8) ? 'millions' : 'milliards'
 
-  return `${n.toFixed(2)} ${w}`
+  return `${parseFloat(n.toFixed(2)).toLocaleString()} ${w}`
 });
 
 new Vue({
