6c5ee646d18ec138294ae102d10a271e7f0e7eeb
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-04-18 00:00:28 +0200

    fix: sources

--- src/Sources.vue
+++ src/Sources.vue
@@ -2,8 +2,8 @@
   <div class="c-sources">
     <h5 class="c-sources__title">Sources</h5>
     <a class="c-sources__link" href="http://www.fakirpresse.info/cice-la-vraie-france-des-assistes">Fakir</a>
-    <a class="c-sources__link" href="http://www.journaldunet.com/business/salaire/classement/metiers/salaires-fonctionnaires">Salaires</a>
-    <a class="c-sources__link" href="http://www.liberation.fr/futurs/2016/02/02/chez-sanofi-les-salaries-ont-moins-la-sante-que-les-actionnaires_1430614">Sanofi</a>
+    <a class="c-sources__link" href="http://www.journaldunet.com/business/salaire/classement/metiers/salaires-fonctionnaires">JDN</a>
+    <a class="c-sources__link" href="http://www.liberation.fr/futurs/2016/02/02/chez-sanofi-les-salaries-ont-moins-la-sante-que-les-actionnaires_1430614">Libération</a>
     <a class="c-sources__link" href="http://www.strategie.gouv.fr/sites/strategie.gouv.fr/files/atoms/files/rapport_cice2016_28095016_ok.pdf">France Stratégie</a>
   </div>
 </template>
