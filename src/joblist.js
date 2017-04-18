0ccb1e00568bb359fc8ae725c05a2f1bf1d8debd
Chloé Gence <chloe.gence@gmail.com>
2017-04-18 17:33:41 +0200

    feminisation

--- src/joblist.js
+++ src/joblist.js
@@ -5,35 +5,35 @@ export const smicMonth = 1568.27
 const jobs = [
   {
     name: 'Infirmier',
-    plural: 'Infirmier(s)',
+    plural: 'Infirmiers.ères',
     costPerMonth: 2100,
     active: true,
     progress: { min: 0, max: totalCICE, value: 5 * 10e8 }
   },
   {
     name: 'Instituteur',
-    plural: 'Instituteurs',
+    plural: 'Instituteurs.trices',
     costPerMonth: 2150,
     active: true,
     progress: { min: 0, max: totalCICE, value: 0 }
   },
   {
     name: 'Animateur périscolaire',
-    plural: 'Animateurs périscolaires',
+    plural: 'Animateurs.trices périscolaires',
     costPerMonth: 820,
     active: true,
     progress: { min: 0, max: totalCICE, value: 0 }
   },
   {
     name: 'Policier municipal',
-    plural: 'Policiers municipaux',
+    plural: 'Policiers.ères municipaux',
     costPerMonth: 1800,
     active: true,
     progress: { min: 0, max: totalCICE, value: 0 }
   },
   {
     name: 'Pompier',
-    plural: 'Pompiers',
+    plural: 'Pompiers.ères',
     costPerMonth: 1800,
     active: true,
     progress: { min: 0, max: totalCICE, value: 0 }
